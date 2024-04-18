1d511dd3abefba4668914b5944081515aa67271a
jlmsdev <joaosouza1135@gmail.com>
2024-04-17 23:31:37 -0300

    Encerrando Projeto, Criptomoeda, pronto para Producao

--- src/pages/detalhe/index.tsx
+++ src/pages/detalhe/index.tsx
@@ -1,4 +1,4 @@
-import style from './detalhe.module.css';
+import styles from './detalhe.module.css';
 
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
@@ -17,8 +17,10 @@ type DataProps = ResponseData | ErrorData;
 export function Detalhe() {
     const { cripto } = useParams();
     const navigate = useNavigate();
+
     const endpointCoin = `https://api.coincap.io/v2/assets/${cripto}`;
     const [detalheCoin, setDetalheCoin] = useState<CoinProps>();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function detalheMoeda() {
@@ -51,6 +53,7 @@ export function Detalhe() {
                     }
 
                     setDetalheCoin((resultadoDetalhe));
+                    setLoading(false);
                 })
 
             } catch (error) {
@@ -63,9 +66,28 @@ export function Detalhe() {
 
     }, [cripto])
 
+    if(loading || !detalheCoin) {
+        return (
+            <div className={styles.containerLoading}>
+                <h3>Carregando detalhes da moeda....</h3>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            {cripto}
+        <div className={styles.container}>
+            <h1 className={styles.center}>{detalheCoin?.name}</h1>
+            <h1 className={styles.center}>{detalheCoin?.symbol}</h1>
+
+            <section className={styles.conteudo}>
+                <img src={`https://assets.coincap.io/assets/icons/${detalheCoin?.symbol.toLocaleLowerCase()}@2x.png`} alt="Logo moeda" />
+
+                <h1>{detalheCoin?.name} | {detalheCoin?.symbol}</h1>
+                <p><strong>Preço: </strong> {detalheCoin?.precoFormatado}</p>
+                <p><strong>Mercado: </strong> {detalheCoin?.precoMercadoFormatado}</p>
+                <p><strong>Volume: </strong> {detalheCoin?.volumeFormatado}</p>
+                <p><strong>Mudança 24hr:</strong> <span className={ Number(detalheCoin?.changePercent24Hr) > 0 ? styles.profit : styles.loss }>{Number(detalheCoin?.changePercent24Hr).toFixed(2)}</span> </p>
+            </section>
         </div>
     )
 }
